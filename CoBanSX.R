cf54a05d77898df270c4fa172b4a2dd490168cff
Sinh vien(pass:123456) <nguyendung.dn308@gmail.com>
2023-12-27 11:23:43 +0700

    update

---
+++ CoBanSX.R
@@ -0,0 +1,33 @@
+#Bai1.1:tinh xac suat de Chon 2 vien bi mau xanh trong 1 hop bi co 3 vien bi xanh va 7 vien bi do
+na=choose(3,2)
+na
+n_omega= choose(10,2)
+n_omega
+p = na/n_omega
+p
+
+#Bai1.2:
+
+p_khongcophepham = choose(2,0) * choose(8,6)/ choose(10,6)
+p_khongcophepham
+p_co1phepham = choose(2,1) * choose(8,5)/choose(10,6); p_co1phepham
+#Xac suat dee khong qua 1 phe pham trong 6 san pham lay ral la:
+p1= p_khongcophepham + p_co1phepham; p1
+
+
+
+#Bai2.1
+#Chon ra 3 qua cau co mau xanh
+a= choose(4,3)/choose(10,3) ;a
+#3 qua mau do
+b= choose(6,3)/choose(10,3);b
+#co 2 qua mau do
+c= choose(6,2) * choose(4,1) / choose(10,3);c
+#2 qua mau xanh
+d= choose(4,2) * choose(6,1) / choose(10,3);d
+
+#bai3.1
+
+
+
+
